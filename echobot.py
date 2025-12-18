e49e553486204500366309d5c050e04ce3925dc7
ingridsmi <ingridsmi@gmail.com>
2025-12-18 14:48:37 -0800

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -15,7 +15,7 @@ st.title("Echo Bot")
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display chat messages from history on app rerun
+# Display chat messages from history on app rerun - session state
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
